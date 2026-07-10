cfc3bf875c50f906707063d61dd1c503b9dd8306
Birhanu Kiros <brightzbrave@gmail.com>
2026-07-10 10:02:30 +0300

    fixed error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let userAge = 25;
 hasDrivingLicense = true; // Use 'true'if they have a license, 'false' if they don't 
 if (userAge < 18) {
     console.log("Sorry, you are too young to drive.");
-} else if (userAge >+ 21 && hasDrivingLicense === trur) {
+} else if (userAge >+ 21 && hasDrivingLicense === true) {
     // This is the bonus condition! (Over 21 AND has a license)
     // We put this BEFORE the 18+ check. Otherwise, a 25-year-old would trigger the "18 or older" condition first, and the computer would stop cheaking!
     console.log("You can drive AND you are also allowed to rent a car!");
